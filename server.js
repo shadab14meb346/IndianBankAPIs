e155215e48c7d9bee4641ff112c100bd76eb486d
Shadab <shadab14meb346@gmail.com>
2020-05-24 11:52:53 +0530

    enhance server and add cors

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
 const express = require('express');
-const http = require('http');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
-const app = express();
-const port = process.env.port || 3001;
 const routes = require('./routes/api_routes');
 const { connectDB } = require('./config/db');
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.json());
-app.use('/', routes);
+
 dotenv.config({ path: './config/config.env' });
 
 connectDB();
+
+const app = express();
+
 // Enable CORS
 app.use(cors());
-app.set('port', port);
-const server = http.createServer(app);
-server.listen(process.env.PORT || 3001, () =>
-	console.log(`API running on localhost:${port}`)
-);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use('/', routes);
+
+const server = app.listen(process.env.PORT || 3001, () => {
+	console.log(`Server running in ${process.env.PORT}`);
+});
